fa116e5e14e2b4b3e6ed733a3c7994c3e7fe7850
IvBut <ivanbutko5@gmail.com>
2020-09-10 00:13:34 +0300

    parse current weather to template object

--- src/services/OpenWeatherService.js
+++ src/services/OpenWeatherService.js
@@ -1,11 +1,21 @@
 import {APP_SETTINGS} from "../constants/ApplicationConfig";
 import axios from 'axios'
+import moment from 'moment';
+import 'moment/locale/ru';
 
 const MEASURMENT_UNITS = {
     metric: 'units=metric',
     imperial: 'units=imperial'
 };
 const excludeParts = ['current', 'minutely','hourly', 'daily'];
+
+export const currentWeatherResponse = {
+    'clouds': 'number',
+    'temp': 'number',
+    'feels_like': 'number',
+    'dt': 'number',
+    'weather': 'object'
+};
 
 class OpenWeatherService {
 
@@ -18,9 +28,10 @@ class OpenWeatherService {
         let url = `${endPoint}/data/2.5/onecall?${params}`;
 
         const response = await axios.get(url);
-        const currentWeather = response.current;
 
-        return response;
+        const currentWeather = createCurrentWeatherObj(response.data.current);
+
+        return currentWeather;
     }
 
     _createParamString(coord,metric, apikey){
@@ -33,5 +44,34 @@ class OpenWeatherService {
     }
 
 }
+
+function createCurrentWeatherObj(obj) {
+   let result  = {};
+    for (const [field, type] of Object.entries(currentWeatherResponse)){
+       if(obj.hasOwnProperty(field) && typeof(obj[field] === type)){
+           result[field] = obj[field];
+
+           if(field === 'clouds'){
+               result[field] = obj[field] + '%'
+           }
+
+           if (field === 'dt') {
+               moment.locale('ru');
+               result[field] = moment(obj[field]*1000)
+                                .format('DD MMM YYYY, HH:mm');
+           }
+
+           if( field === 'weather' && Array.isArray(obj[field])){
+               result[field] = {
+                   ...obj[field][0]
+               }
+           }
+
+
+       }
+   }
+   return result;
+}
+
 
 export const openWeatherService = new OpenWeatherService();
